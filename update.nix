dce8a6ea3df8bb6ec63b7eb5be0bca0ec74fd4f3
heath robertson <codehappens@toilethill.io>
2020-06-10 12:10:49 +0000

    Rebuilt site and updated using update.nix
    What these changes address:

---
+++ update.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "base-jekyll-builder";
+
+  buildInputs = [
+    ruby
+    jekyll
+    nodejs
+  ];
+  shellHook = ''
+    cd blog/
+    bundler package --no-install --path vendor
+    rm -rf .bundle/ vendor/
+    $(nix-build '<nixpkgs>' -A bundix)/bin/bundix
+    rm result
+  '';
+
+}
+
+######
+#NOTES
+######
+# update.nix is used to rebuild the Jekyll environment after updates to _config.yml or Gemfile.
+# This file borrows heavily from: http://stesie.github.io/2016/08/nixos-github-pages-env
+
+# docker run --rm --volumes-from=nix -it -v=$(pwd):/home -w=/home nixos/nix nix-shell /home/update.nix
